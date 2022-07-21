0ade5ce29489eccd9867f71a3e199d28c3efc064
anandadwipra <anandabiru04@gmail.com>
2022-07-21 19:02:13 +0700

    add docker install

--- install/docker.sh
+++ install/docker.sh
@@ -4,5 +4,6 @@ apt-get install apt-transport-https ca-certificates curl gnupg-agent software-pr
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  
 sudo apt-key fingerprint 0EBFCD88  
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y  
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y  
+sudo apt-get update -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
